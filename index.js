c743f7ac8cf29abc42189b38c47df78024aed42e
JessieCheng <hamy820326@gmail.com>
2021-05-24 00:25:34 +0800

    feat: add product list feature

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const app = {
   data () {
     return {
+      cookieName: 'hexschoolvue',
+      API: axios.create({
+        baseURL: `https://vue3-course-api.hexschool.io/api/jessiemosbi`,
+        headers: { 'Authorization': document.cookie.replace(/(?:(?:^|.*;\s*)hexschoolvue\s*\=\s*([^;]*).*$)|^.*$/, "$1") }
+      }),
+      products: []
     }
   },
 
   methods: {
+    getData () {
+      this.API.get(`/admin/products?page=1`)
+        .then(res => {
+          if (!res.data.success) {
+            alert('獲取產品列表資料失敗！');
+            return;
+          }
+
+          this.products = res.data.products;
+        })
+        .catch(err => console.dir(err))
+    },
   },
 
   created () {
+    this.getData();
   }
 }
 
