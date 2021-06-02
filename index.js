97ad15167e55fab35e4e7e80ec71988ca4e501cb
JessieCheng <hamy820326@gmail.com>
2021-06-02 21:47:09 +0800

    refactor: add method on pagination component

--- index.js
+++ index.js
@@ -167,19 +167,24 @@ const app = Vue.createApp({
 // TODO: 有空再套一下之前有寫過的 pagination 中間固定只顯示幾頁（先把其他 modal 拆成 component 要緊！）
 
 app.component('pagination', {
+  methods: {
+    clickPage (page) {
+      this.$emit('changePage', page);
+    }
+  },
   props: ['totalPages', 'currentPage', 'hasPrePage', 'hasNextPage'],
   template: `
     <nav aria-label="Page navigation example">
       <ul class="pagination">
-        <li class="page-item" v-if="hasPrePage" @click.prevent="$emit('changePage', currentPage-1)">
+        <li class="page-item" v-if="hasPrePage" @click.prevent="clickPage(currentPage-1)">
           <a class="page-link" href="#" aria-label="Previous">
             <span aria-hidden="true">&laquo;</span>
           </a>
         </li>
         <li class="page-item" :class="{active: currentPage === number}" v-for="number in totalPages" :key="number">
-          <a class="page-link" href="javascript:;" @click="$emit('changePage', number)">{{number}}</a>
+          <a class="page-link" href="javascript:;" @click="clickPage(number)">{{number}}</a>
         </li>
-        <li class="page-item" v-if="hasNextPage" @click.prevent="$emit('changePage', currentPage+1)">
+        <li class="page-item" v-if="hasNextPage" @click.prevent="clickPage(currentPage+1)">
           <a class="page-link" href="#" aria-label="Next">
             <span aria-hidden="true">&raquo;</span>
           </a>
